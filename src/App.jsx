356f86bc4cfa871e150257d242b8413b60235512
JulianAballay <juliancabj1210@gmail.com>
2025-12-04 02:14:02 -0300

    agrego el maquetado de menu. Agrego unos iconos para el navbarBrand y para el iniciar sesion. Ademass agrego un colora orange para el icono y el button mismo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.min.css'
+import Menu from './components/common/Menu'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      
+      <Menu></Menu>
     </>
   )
 }
---
+++ src/components/common/Menu.jsx
@@ -0,0 +1,20 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+const Menu = () => {
+  return (
+    <Navbar expand="lg" bg="light" data-bs-theme="light">
+      <Container>
+        <Navbar.Brand href="#home"><i className="bi bi-fork-knife fs-3 colorEstilo"></i> Mi blog de recetas</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link className="buttonMenu rounded text-white" href="#home">Ver Recetas</Nav.Link>
+            <Nav.Link href="#link"  className="ms-3"><i className="bi bi-arrow-bar-right"></i> Iniciar Sesión</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Menu;
