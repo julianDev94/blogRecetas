16e05a0ef71409fedbcfbdf7ab80424d778f521b
JulianAballay <juliancabj1210@gmail.com>
2025-12-13 02:10:02 -0300

    creo el componente inicio y agrego maquetado en el mismo con un container y un parrafo para el titulo

---
+++ src/components/pages/Inicio.jsx
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+
+const Inicio = () => {
+    return (
+        <section className="seccionPricipal">
+            <Container className="my-3">
+                <p className="fs-5">Explora Nuestras Recetas</p>
+                
+            </Container>
+        </section>
+    );
+};
+
+export default Inicio;
