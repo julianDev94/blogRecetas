92b02a5563a9f704c74ae92a1deddbf7925821f4
JulianAballay <juliancabj1210@gmail.com>
2025-12-14 21:51:23 -0300

    agrego algo del maquetado de la card junto con la transicion con un hover para la misma

---
+++ src/components/pages/recetas/CardReceta.jsx
@@ -0,0 +1,23 @@
+import { Card } from "react-bootstrap";
+const CardReceta = () => {
+  return (
+    <Card className="cardTransicion">
+      <Card.Img variant="top" src="holder.js/100px160" />
+      <Card.Body>
+        <Card.Title>Nombre receta</Card.Title>
+        <Card.Text>Descripcion corta</Card.Text>
+        <div className="d-flex">
+          <p className="me-3">
+            <i class="bi bi-clock-history"></i> Duracion preparacion
+          </p>
+          <p>
+            <i class="bi bi-people"></i> 
+            Cantidad personas
+          </p>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default CardReceta;
