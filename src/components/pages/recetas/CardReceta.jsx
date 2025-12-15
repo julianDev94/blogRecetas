d075cae83f145801b6977d4115d9b20166de49d7
JulianAballay <juliancabj1210@gmail.com>
2025-12-15 00:05:38 -0300

    agrego un margin top al card title

--- src/components/pages/recetas/CardReceta.jsx
+++ src/components/pages/recetas/CardReceta.jsx
@@ -2,11 +2,11 @@ import { Card } from "react-bootstrap";
 
 const CardReceta = () => {
   return (
-    <Card className="cardTransicion">
+    <Card className="cardTransicion border-0">
       <Card.Img variant="top" src="https://images.pexels.com/photos/691114/pexels-photo-691114.jpeg" alt="Imagen de la receta no disponible" />
       <Card.Body>
         <span className="bg-primary rounded px-2 py-1 text-white">Categoria</span>
-        <Card.Title>Nombre receta</Card.Title>
+        <Card.Title className="mt-3">Nombre receta</Card.Title>
         <Card.Text>Descripcion corta</Card.Text>
         <div className="d-flex">
           <p className="me-3">
