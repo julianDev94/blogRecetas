82304c65a0fefe2c88f13baa012a7952e8b11f51
JulianAballay <juliancabj1210@gmail.com>
2025-12-14 23:25:47 -0300

    le doy formato al componente

--- src/components/pages/recetas/CardReceta.jsx
+++ src/components/pages/recetas/CardReceta.jsx
@@ -3,7 +3,7 @@ import { Card } from "react-bootstrap";
 const CardReceta = () => {
   return (
     <Card className="cardTransicion">
-      <Card.Img variant="top" src="https://images.pexels.com/photos/691114/pexels-photo-691114.jpeg" />
+      <Card.Img variant="top" src="https://images.pexels.com/photos/691114/pexels-photo-691114.jpeg" alt="Imagen de la receta no disponible" />
       <Card.Body>
         <Card.Title>Nombre receta</Card.Title>
         <Card.Text>Descripcion corta</Card.Text>
@@ -12,8 +12,7 @@ const CardReceta = () => {
             <i class="bi bi-clock-history"></i> Duracion preparacion
           </p>
           <p>
-            <i class="bi bi-people"></i> 
-            Cantidad personas
+            <i class="bi bi-people"></i> Cantidad personas
           </p>
         </div>
       </Card.Body>
