96772679bea8ccc6af47cf1d9047ce88eaa9451b
JulianAballay <juliancabj1210@gmail.com>
2025-12-14 23:55:51 -0300

    agrego diseño para las categorias del inicio

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,10 +1,13 @@
-import { Col, Container, Row, Card } from "react-bootstrap";
+import { Col, Container, Row, Stack, Badge } from "react-bootstrap";
 import CardReceta from "./recetas/CardReceta";
 const Inicio = () => {
     return (
         <section className="seccionPricipal">
             <Container className="my-3">
                 <p className="fs-5">Explora Nuestras Recetas</p>
+                <div className="mb-3">
+                    <span className="px-3 py-2 rounded-pill text-white buttonMenu">Categorias</span>
+                </div>
                 <Row xs={1} md={3} className="g-4">
                     <Col>
                         <CardReceta></CardReceta>
--- src/components/pages/recetas/CardReceta.jsx
+++ src/components/pages/recetas/CardReceta.jsx
@@ -5,14 +5,15 @@ const CardReceta = () => {
     <Card className="cardTransicion">
       <Card.Img variant="top" src="https://images.pexels.com/photos/691114/pexels-photo-691114.jpeg" alt="Imagen de la receta no disponible" />
       <Card.Body>
+        <span className="bg-primary rounded px-2 py-1 text-white">Categoria</span>
         <Card.Title>Nombre receta</Card.Title>
         <Card.Text>Descripcion corta</Card.Text>
         <div className="d-flex">
           <p className="me-3">
-            <i class="bi bi-clock-history"></i> Duracion preparacion
+            <i className="bi bi-clock-history"></i> Duracion preparacion
           </p>
           <p>
-            <i class="bi bi-people"></i> Cantidad personas
+            <i className="bi bi-people"></i> Cantidad personas
           </p>
         </div>
       </Card.Body>
