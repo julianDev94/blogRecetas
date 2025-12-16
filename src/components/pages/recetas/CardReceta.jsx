423ef7020cc77aeb7477640b4fa64b58ffcb4802
JulianAballay <juliancabj1210@gmail.com>
2025-12-16 11:43:43 -0300

    modifico el efecto de la cardReceta

--- src/components/pages/recetas/CardReceta.jsx
+++ src/components/pages/recetas/CardReceta.jsx
@@ -2,7 +2,7 @@ import { Card } from "react-bootstrap";
 
 const CardReceta = () => {
   return (
-    <Card className="cardTransicion border-0">
+    <Card className="cardTransicion">
       <Card.Img variant="top" src="https://images.pexels.com/photos/691114/pexels-photo-691114.jpeg" alt="Imagen de la receta no disponible" />
       <Card.Body>
         <span className="bg-primary rounded px-2 py-1 text-white">Categoria</span>
