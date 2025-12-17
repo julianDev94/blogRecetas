0f13db2e1525acf44175d36837d0debff7eb8231
JulianAballay <juliancabj1210@gmail.com>
2025-12-16 21:15:05 -0300

    modifico el diseño de la columna del titulo agregando una imagen y la descricpion pequeña del titulo

--- src/components/pages/recetas/ItemReceta.jsx
+++ src/components/pages/recetas/ItemReceta.jsx
@@ -2,7 +2,13 @@
 const ItemReceta = () => {
   return (
     <tr>
-      <td>1</td>
+      <td>
+        <div className="d-flex">
+            <img src="https://images.pexels.com/photos/691114/pexels-photo-691114.jpeg" alt="" className="img-fila rounded" />
+            <div className="ms-3"><p>Nombre receta</p>
+            <p className="text-body-secondary">Descripcion breve</p></div>
+        </div>
+      </td>
       <td>Mark</td>
       <td>Otto</td>
       <td>@mdo</td>
