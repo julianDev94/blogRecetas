659ba904aedd02dff34a0099292c2e25b8f03c36
JulianAballay <juliancabj1210@gmail.com>
2025-12-17 21:25:02 -0300

    agrego al css el resize none al textarea

--- src/components/pages/recetas/FormularioReceta.jsx
+++ src/components/pages/recetas/FormularioReceta.jsx
@@ -8,25 +8,25 @@ const FormularioReceta = () => {
     setIngredientes([...ingredientes, ""]);
   };
 
-  const eliminarIngrediente =(posicion)=>{
-    const ingredientesNuevos = ingredientes.filter((_, i)=> i !== posicion);
+  const eliminarIngrediente = (posicion) => {
+    const ingredientesNuevos = ingredientes.filter((_, i) => i !== posicion);
     setIngredientes(ingredientesNuevos);
-  }
+  };
 
-  const agregarPasos = () =>{
+  const agregarPasos = () => {
     setPasos([...pasos, ""]);
-  }
+  };
 
-  const eliminarPaso = (posicion)=>{
-    const pasosNuevos = pasos.filter((_,pos)=>pos !== posicion)
+  const eliminarPaso = (posicion) => {
+    const pasosNuevos = pasos.filter((_, pos) => pos !== posicion);
     setPasos(pasosNuevos);
-  }
+  };
 
   return (
     <section className="seccionPricipal">
-      <Container className="mt-4">
+      <Container className="my-4">
         <h5>Alta de recetas</h5>
-        <Form className="bg-white p-3 rounded shadow-lg">
+        <Form className="bg-white p-4 rounded shadow-lg">
           <Row className="mb-3">
             <Form.Group as={Col} controlId="formularioTitulo">
               <Form.Label>Título *</Form.Label>
@@ -78,23 +78,26 @@ const FormularioReceta = () => {
                 </Button>
               </div>
             </div>
-            {
-              ingredientes.map((_, posicion)=>
+            {ingredientes.map((_, posicion) => (
               <div className="d-flex mb-3" key={posicion}>
-              <Form.Control type="text" placeholder={`Ingrediente ${posicion+1}`} />
-              <div className="ms-3">
-                {
-                  ingredientes.length > 1 && (<Button variant="danger" onClick={()=>eliminarIngrediente(posicion)}>
-                  <i className="bi bi-x-lg"></i>
-                </Button>)
-
-                }
-                
+                <Form.Control
+                  type="text"
+                  placeholder={`Ingrediente ${posicion + 1}`}
+                />
+                <div className="ms-3">
+                  {ingredientes.length > 1 && (
+                    <Button
+                      variant="danger"
+                      onClick={() => eliminarIngrediente(posicion)}
+                    >
+                      <i className="bi bi-x-lg"></i>
+                    </Button>
+                  )}
+                </div>
               </div>
-            </div>)
-            }
+            ))}
           </Form.Group>
-           <Form.Group controlId="formularioPasos" className="mb-3">
+          <Form.Group controlId="formularioPasos" className="mb-3">
             <div className="d-flex mb-2">
               <Form.Label className="me-auto">Pasos para receta *</Form.Label>
               <div>
@@ -103,22 +106,34 @@ const FormularioReceta = () => {
                 </Button>
               </div>
             </div>
-            {
-              pasos.map((_, posicion)=>
+            {pasos.map((_, posicion) => (
               <div className="d-flex mb-3" key={posicion}>
-                <p className="btn btn-danger me-3">{posicion+1}</p>
-              <Form.Control type="text" placeholder={`Paso ${posicion+1}`} />
-              <div className="ms-3">
-                {
-                  pasos.length > 1 && (<Button variant="danger" onClick={()=>eliminarPaso(posicion)}>
-                  <i className="bi bi-x-lg"></i>
-                </Button>)
-                }
-                
+                <p className="btn btn-danger me-3">{posicion + 1}</p>
+                <Form.Control
+                  type="text"
+                  placeholder={`Paso ${posicion + 1}`}
+                />
+                <div className="ms-3">
+                  {pasos.length > 1 && (
+                    <Button
+                      variant="danger"
+                      onClick={() => eliminarPaso(posicion)}
+                    >
+                      <i className="bi bi-x-lg"></i>
+                    </Button>
+                  )}
+                </div>
               </div>
-            </div>)
-            }
+            ))}
           </Form.Group>
+          <div className="d-flex gap-3 justify-content-center mt-3">
+            <Button type="submit" variant="danger" size="lg">
+              Crear receta
+            </Button>
+            <Button type="button" variant="secondary" size="lg">
+              Cancelar
+            </Button>
+          </div>
         </Form>
       </Container>
     </section>
