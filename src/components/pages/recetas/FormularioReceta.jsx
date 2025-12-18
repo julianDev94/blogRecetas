5edf8e86d0937557ff55e967ccd01844c351bcfc
JulianAballay <juliancabj1210@gmail.com>
2025-12-17 23:18:46 -0300

    agrego un form text para mostrar los errores que se cometen al ingresar mal los datos de los input

--- src/components/pages/recetas/FormularioReceta.jsx
+++ src/components/pages/recetas/FormularioReceta.jsx
@@ -8,7 +8,7 @@ const FormularioReceta = () => {
     handleSubmit,
     formState: { errors },
     reset,
-    setValue
+    setValue,
   } = useForm();
 
   const [ingredientes, setIngredientes] = useState([""]);
@@ -47,85 +47,137 @@ const FormularioReceta = () => {
           <Row className="mb-3">
             <Form.Group as={Col} controlId="formularioTitulo">
               <Form.Label>Título *</Form.Label>
-              <Form.Control type="text" placeholder="Nombre de la receta" {...register("titulo",{
-                required:"El titulo de la receta es obligatorio",
-                minLength: {
-                  value: 4,
-                  message: "El titulo de la receta tiene menos de 4 caracteres"
-                },
-                maxLength:{
-                  value: 20,
-                  message: "El titulo de la receta tiene más"
-                }})} />
+              <Form.Control
+                type="text"
+                placeholder="Nombre de la receta"
+                {...register("titulo", {
+                  required: "El titulo de la receta es obligatorio",
+                  minLength: {
+                    value: 4,
+                    message:
+                      "El titulo de la receta tiene menos de 4 caracteres",
+                  },
+                  maxLength: {
+                    value: 20,
+                    message: "El titulo de la receta tiene más",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.titulo?.message}
+              </Form.Text>
             </Form.Group>
 
             <Form.Group as={Col} controlId="formularioCategoria">
               <Form.Label>Categoria *</Form.Label>
-              <Form.Select aria-label="Default select example" {...register("categoria", {required: "La categoria es obligatoria"})}>
+              <Form.Select
+                aria-label="Default select example"
+                {...register("categoria", {
+                  required: "La categoria es obligatoria",
+                })}
+              >
                 <option>Seleccione la categoria</option>
                 <option value="Principal">Principal</option>
                 <option value="Postre">Postre</option>
                 <option value="Ensalada">Ensalada</option>
               </Form.Select>
+              <Form.Text className="text-danger">
+                {errors.categoria?.message}
+              </Form.Text>
             </Form.Group>
           </Row>
           <Form.Group className="mb-3" controlId="textAreaDescripcion">
             <Form.Label>Descripcion breve *</Form.Label>
-            <Form.Control as="textarea" rows={3} {...register("descripcionBreve",{
-              required: "La descripcion es obligatoria",
-              minLength:{
-                value: 4,
-                message: "La descripcion ingresada tiene menos de 4 caracteres"
-              },
-              maxLength:{
-                value: 30,
-                message: "La descripcion ingresada tiene más de 30 caracteres"
-              }
-            })}/>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              {...register("descripcionBreve", {
+                required: "La descripcion es obligatoria",
+                minLength: {
+                  value: 4,
+                  message:
+                    "La descripcion ingresada tiene menos de 4 caracteres",
+                },
+                maxLength: {
+                  value: 30,
+                  message:
+                    "La descripcion ingresada tiene más de 30 caracteres",
+                },
+              })}
+            />
+            <Form.Text className="text-danger">
+              {errors.descripcionBreve?.message}
+            </Form.Text>
           </Form.Group>
           <Row className="mb-3">
             <Form.Group as={Col} controlId="formularioTiempoPrep">
               <Form.Label>Tiempo de Prep. Minutos*</Form.Label>
-              <Form.Control type="number" placeholder="Por ejemplo: 20" {...register("tiempoPrep", {
-                required: "El tiempo de preparacion es obligatorio",
-                min:{
-                  value: 1,
-                  message: "El tiempo de preparacion debe ser mayor a 1 minuto"
-                },
-                max:{
-                  value: 500,
-                  message: "El tiempo de preparacion debe ser menor a 500 minutos"
-                }
-              })} />
+              <Form.Control
+                type="number"
+                placeholder="Por ejemplo: 20"
+                {...register("tiempoPrep", {
+                  required: "El tiempo de preparacion es obligatorio",
+                  min: {
+                    value: 1,
+                    message:
+                      "El tiempo de preparacion debe ser mayor a 1 minuto",
+                  },
+                  max: {
+                    value: 500,
+                    message:
+                      "El tiempo de preparacion debe ser menor a 500 minutos",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.tiempoPrep?.message}
+              </Form.Text>
             </Form.Group>
 
             <Form.Group as={Col} controlId="formularioTiempoCoccion">
               <Form.Label>Tiempo Cocción. Minutos*</Form.Label>
-              <Form.Control type="number" placeholder="Por ejemplo: 20" {...register("tiempoCoccion",{
-                required: "El tiempo de coccion es obligatorio",
-                min:{
-                  value: 1,
-                  message: "El tiempo de coccion debe superior a 1 minuto"
-                },
-                max: {
-                  value: 500,
-                  message: "El tiempo de coccion debe ser menor a 500 minutos"
-                }
-              })} />
+              <Form.Control
+                type="number"
+                placeholder="Por ejemplo: 20"
+                {...register("tiempoCoccion", {
+                  required: "El tiempo de coccion es obligatorio",
+                  min: {
+                    value: 1,
+                    message: "El tiempo de coccion debe superior a 1 minuto",
+                  },
+                  max: {
+                    value: 500,
+                    message:
+                      "El tiempo de coccion debe ser menor a 500 minutos",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.tiempoCoccion?.message}
+              </Form.Text>
             </Form.Group>
             <Form.Group as={Col} controlId="formularioPorciones">
               <Form.Label>Porciones *</Form.Label>
-              <Form.Control type="number" placeholder="Por ejemplo: 5" {...register("porciones", {
-                required: "La cantidad de porciones es obligatoria",
-                min:{
-                  value: 1,
-                  message: "La cantidad de porciones debe ser mayor o igual a 1"
-                },
-                max:{
-                  value: 20,
-                  message: "La cantidad de porciones debe ser menor o igual a 20"
-                }
-              })} />
+              <Form.Control
+                type="number"
+                placeholder="Por ejemplo: 5"
+                {...register("porciones", {
+                  required: "La cantidad de porciones es obligatoria",
+                  min: {
+                    value: 1,
+                    message:
+                      "La cantidad de porciones debe ser mayor o igual a 1",
+                  },
+                  max: {
+                    value: 20,
+                    message:
+                      "La cantidad de porciones debe ser menor o igual a 20",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.porciones?.message}
+              </Form.Text>
             </Form.Group>
           </Row>
           <Form.Group controlId="formularioURLImagen" className="mb-3">
@@ -134,14 +186,16 @@ const FormularioReceta = () => {
               type="url"
               placeholder="Por ejemplo: https://imagen.com"
               {...register("urlImagen", {
-
                 required: "La URL de la imagen es obligatoria",
-                pattern:{
+                pattern: {
                   value: /^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/,
-                  message: "La direccion de la url ingresada no es valida"
-                }
+                  message: "La direccion de la url ingresada no es valida",
+                },
               })}
             />
+            <Form.Text className="text-danger">
+                {errors.urlImagen?.message}
+              </Form.Text>
           </Form.Group>
           <Form.Group controlId="formularioIngredientes">
             <div className="d-flex mb-2">
@@ -159,16 +213,21 @@ const FormularioReceta = () => {
                   placeholder={`Ingrediente ${posicion + 1}`}
                   {...register("ingredientes", {
                     required: "El ingrediente es obligatorio",
-                    minLength:{
+                    minLength: {
                       value: 3,
-                      message: "La cantidad de caracteres debe ser mayor o igual 3"
+                      message:
+                        "La cantidad de caracteres debe ser mayor o igual 3",
                     },
-                    maxLength:{
+                    maxLength: {
                       value: 30,
-                      message: "La cantidad de caracteres debe ser menor o igual a 30"
-                    }
+                      message:
+                        "La cantidad de caracteres debe ser menor o igual a 30",
+                    },
                   })}
                 />
+                <Form.Text className="text-danger">
+                {errors.ingredientes?.message}
+              </Form.Text>
                 <div className="ms-3">
                   {ingredientes.length > 1 && (
                     <Button
@@ -199,16 +258,21 @@ const FormularioReceta = () => {
                   placeholder={`Paso ${posicion + 1}`}
                   {...register("pasos", {
                     required: "Los pasos son obligatorios",
-                    minLength:{
+                    minLength: {
                       value: 3,
-                      message: "La cantidad de caracteres debe ser mayor o igual a 3"
+                      message:
+                        "La cantidad de caracteres debe ser mayor o igual a 3",
                     },
-                    maxLength:{
+                    maxLength: {
                       value: 50,
-                      message: "La cantidad de caracteres debe ser menor o igual a 30"
-                    }
+                      message:
+                        "La cantidad de caracteres debe ser menor o igual a 30",
+                    },
                   })}
                 />
+                <Form.Text className="text-danger">
+                {errors.pasos?.message}
+              </Form.Text>
                 <div className="ms-3">
                   {pasos.length > 1 && (
                     <Button
