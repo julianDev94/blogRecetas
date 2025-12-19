945ff45c81c5bf696fd450689cda7d68efb71da9
JulianAballay <juliancabj1210@gmail.com>
2025-12-19 14:03:08 -0300

    configuro los componente navlink de react router para el menu y un link para el logo del blog

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,16 +1,34 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link, NavLink } from "react-router";
 
 const Menu = () => {
   return (
-    <Navbar expand="lg"  data-bs-theme="light">
+    <Navbar expand="lg" data-bs-theme="light">
       <Container>
-        <Navbar.Brand href="#home"><i className="bi bi-fork-knife fs-3 colorEstilo"></i> Mi blog de recetas</Navbar.Brand>
+        <Navbar.Brand as={Link} to={"/"}>
+          <i className="bi bi-fork-knife fs-3 colorEstilo"></i> Mi blog de
+          recetas
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link className="buttonMenu rounded text-white text-center" href="#home">Ver Recetas</Nav.Link>
-             <Nav.Link className="buttonMenu rounded text-white text-center ms-3" href="#home">Panel de Administrador</Nav.Link>
-            <Nav.Link href="#link"  className="ms-3"><i className="bi bi-arrow-bar-right"></i> Iniciar Sesión</Nav.Link>
+            <NavLink
+              to="/"
+              end
+              className="buttonMenu nav-link rounded text-white text-center"
+            >
+              Ver Recetas
+            </NavLink>
+            <NavLink
+              to="/administrador"
+              end
+              className="buttonMenu nav-link rounded text-white text-center ms-3"
+            >
+              Panel de Administrador
+            </NavLink>
+            <NavLink to="/login" end className="ms-3 nav-link">
+              <i className="bi bi-arrow-bar-right"></i> Iniciar Sesión
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
