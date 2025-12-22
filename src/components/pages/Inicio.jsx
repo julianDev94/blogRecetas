001cd8f605fb1a5b660671c89f476e4b7f241e13
JulianAballay <juliancabj1210@gmail.com>
2025-12-21 23:52:10 -0300

    creo el componente modalReceta para cuando haga click en el componente cardReceta se abra un modal del mismo mostrando los datos de la receta

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,21 +1,25 @@
-import { Col, Container, Row, Stack, Badge } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import CardReceta from "./recetas/CardReceta";
+import { useState } from "react";
+
 const Inicio = () => {
-    return (
-        <section className="seccionPricipal">
-            <Container className="my-3">
-                <p className="fs-5">Explora Nuestras Recetas</p>
-                <div className="mb-3">
-                    <span className="px-3 py-2 rounded-pill text-white buttonMenu">Categorias</span>
-                </div>
-                <Row xs={1} md={3} className="g-4">
-                    <Col>
-                        <CardReceta></CardReceta>
-                    </Col>
-                </Row>
-            </Container>
-        </section>
-    );
+  return (
+    <section className="seccionPricipal">
+      <Container className="my-3">
+        <p className="fs-5">Explora Nuestras Recetas</p>
+        <div className="mb-3">
+          <span className="px-3 py-2 rounded-pill text-white buttonMenu">
+            Categorias
+          </span>
+        </div>
+        <Row xs={1} md={3} className="g-4">
+          <Col>
+            <CardReceta></CardReceta>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 };
 
 export default Inicio;
