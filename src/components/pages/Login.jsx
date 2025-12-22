ae5b3746e62ae862ed9ea029a9413215c90e0e0c
JulianAballay <juliancabj1210@gmail.com>
2025-12-22 02:15:44 -0300

    realizo las validaciones con reack hook form en el formulario y le agrego un form text para mostrar los erroress en caso que no este validado los datoss ingresados

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,6 +1,16 @@
 import { Container, Button, Form, Card } from "react-bootstrap";
-
+import { useForm } from "react-hook-form";
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const validacionLogin = (datos) => {
+    console.log(datos);
+  };
+
   return (
     <section className="seccionPricipal">
       <Container className="d-flex justify-content-center my-5 ">
@@ -9,7 +19,7 @@ const Login = () => {
          col-sm-10 col-lg-5 col-md-8 border border-0"
         >
           <Card.Body>
-            <Form>
+            <Form onSubmit={handleSubmit(validacionLogin)}>
               <div className="text-center">
                 <p>
                   <i className="bi bi-lock-fill fs-2 colorEstilo"></i>
@@ -20,7 +30,25 @@ const Login = () => {
 
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Usuario</Form.Label>
-                <Form.Control type="text" placeholder="Ingresa tu usuario" />
+                <Form.Control
+                  type="text"
+                  placeholder="Ingresa tu usuario"
+                  {...register("usuario", {
+                    required: "El usuario es obligatorio",
+                    minLength: {
+                      value: 5,
+                      message:
+                        "El usuario ingresado debe tener más de 5 caracteres",
+                    },
+                    maxLength: {
+                      value: 8,
+                      message: "El usuario ingresado debe al menos 8 caracteres",
+                    },
+                  })}
+                />
+                <Form.Text className="text-danger">
+                  {errors.usuario?.message}
+                </Form.Text>
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -28,7 +56,18 @@ const Login = () => {
                 <Form.Control
                   type="password"
                   placeholder="Ingresa tu contraseña"
+                  {...register("contrasenia", {
+                    required: "La contraseña es obligatoria",
+                    pattern: {
+                      value: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
+                      message:
+                        "La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula. NO puede tener otros símbolos",
+                    },
+                  })}
                 />
+                <Form.Text className="text-danger">
+                  {errors.contrasenia?.message}
+                </Form.Text>
               </Form.Group>
               <div className="d-grid gap-2">
                 <Button type="submit" size="lg">
