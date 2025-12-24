e1ac2a534ab721d8bb486ffbfcbbac97cbb3e776
JulianAballay <juliancabj1210@gmail.com>
2025-12-24 13:34:05 -0300

    creo un metodo get en el archivo consultassAPI para obtener del endpoint el array de recetas

--- src/helpers/consultasAPI.js
+++ src/helpers/consultasAPI.js
@@ -15,6 +15,16 @@ export const altaReceta = async (receta) => {
   }
 };
 
+export const obtenerRecetas = async () => {
+  try {
+    const respuesta = await fetch(apiRecetas);
+    const datosRecetas = await respuesta.json();
+    return  datosRecetas;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const admin = {
   usuario: "admin",
   contrasenia: "1234Admin",
