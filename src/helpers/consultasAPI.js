f712e9c301c4cf95538e149408db45f5d6c8eae3
JulianAballay <juliancabj1210@gmail.com>
2026-01-12 22:12:10 -0300

    creo el metodo modificarReceta en el archivo consultasApi para modificar una receta seleccionada, enviandole por parametros el id y el producto a modificar

--- src/helpers/consultasAPI.js
+++ src/helpers/consultasAPI.js
@@ -27,6 +27,21 @@ export const obtenerRecetas = async () => {
   }
 };
 
+export const modificarReceta = async (id, productoAModificar)=>{
+  try {
+    const respuesta = await fetch(apiRecetas + `/${id}`, {
+      method: "PUT",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify(productoAModificar);
+    });
+    return respuesta;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 const admin = {
   usuario: "admin",
   contrasenia: "1234Admin",
