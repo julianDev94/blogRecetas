108a799f594ca6409d7f5a2697de9fd035812e92
JulianAballay <juliancabj1210@gmail.com>
2026-03-03 21:14:50 -0300

    modifico el codigo del componente Row para mostrar las recetas con las respectivas Cards de las recetas

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -13,24 +13,30 @@ const Inicio = () => {
             Categorias
           </span>
         </div>
-        <Row xs={1} md={2} lg={4} className="g-4">
-          {isPending ? (
-            <div className="d-flex flex-column justify-content-center align-items-center w-100">
-              <Spinner animation="border" variant="danger" />
-              <span className="fw-bold">Cargando recetas...</span>
-            </div>
-          ) : isError? (<div className="d-flex flex-column justify-content-center align-items-center w-100">
-              <p className="fw-bold text-warning ">Error al cargas las recetas.</p>
-            </div>) : recetas.length === 0? (<div className="d-flex flex-column justify-content-center align-items-center w-100">
-              <span className="fw-bold">No hay recetas disponibles por el momento.</span>
-            </div>) : (
-            recetas.map((receta) => (
+        {isPending ? (
+          <div className="d-flex flex-column justify-content-center align-items-center w-100">
+            <Spinner animation="border" variant="danger" />
+            <span className="fw-bold">Cargando recetas...</span>
+          </div>
+        ) : isError ? (
+          <div className="d-flex flex-column justify-content-center align-items-center w-100">
+            <p className="fw-bold text-danger">Error al cargas las recetas.</p>
+          </div>
+        ) : recetas.length === 0 ? (
+          <div className="d-flex flex-column justify-content-center align-items-center w-100">
+            <span className="fw-bold text-warning">
+              No hay recetas disponibles por el momento.
+            </span>
+          </div>
+        ) : (
+          <Row xs={1} md={2} lg={4} className="g-4">
+            {recetas.map((receta) => (
               <Col key={receta._id}>
                 <CardReceta receta={receta} key={receta._id} />
               </Col>
-            ))
-          )}
-        </Row>
+            ))}
+          </Row>
+        )}
       </Container>
     </section>
   );
